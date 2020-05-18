9b0cdb462e0a32a58c2fe4f42f2e3a5f15c6d708
Nagarajan17699 <nagarajan17699@yahoo.in>
2020-05-18 20:03:50 +0530

    18May20

---
+++ balancing_paranthesis.cpp
@@ -0,0 +1,44 @@
+/*
+PROGRAM STATEMENT:  Balance the given Paranthesis
+			
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string str;
+    char temp='(';
+   
+    while((temp=='(') || (temp==')')) //this WHILE block can be replaced by "cin>>str". This while loop has been written due to some problem during getting the input from hackerearth site.
+    {
+        temp='*';
+        cin>>temp;
+        //cout<<temp;
+        if(temp != '*')
+        str+=temp;
+    }
+   //cout<<str;
+    int count=0;
+    stack<char> s;
+    //cout<<str;
+    for(int i=0;i<str.length();i++)
+    {
+        //cout<<str[i];
+        if(str[i]=='(')
+            s.push('(');
+        else if(str[i]==')' && (!s.empty()))
+        {
+            s.pop();
+            count++;
+        }
+        else
+        {
+            cout<<"-1"<<endl;
+            return 0;
+        }
+    }
+    cout<<count<<endl;
+    return 0;
+}
